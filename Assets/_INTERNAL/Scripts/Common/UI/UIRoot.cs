c51f32b83e544f4ac4cd276a32579f74ee9e0e2c
Waste Human <drugula19@outlook.com>
2026-05-27 20:42:28 +0700

    New architecture (not ai)

---
+++ Assets/_INTERNAL/Scripts/Common/UI/UIRoot.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace Common.UI
+{
+    public class UIRoot : MonoBehaviour
+    {
+        [field: SerializeField] public Transform ScreenLayer { get; private set; }
+        [field: SerializeField] public Transform PopupLayer { get; private set; }
+        [field: SerializeField] public Transform OverlayLayer { get; private set; }
+    }
+}
